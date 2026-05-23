6964b1730a14b48b7fbaf3f04b53f66605e4b8d3
Najdain <najdaintafdhila@gmail.com>
2026-05-24 02:23:57 +0700

    feat: update URL API

--- web-app/backend/routes/skills.routes.js
+++ web-app/backend/routes/skills.routes.js
@@ -3,7 +3,10 @@ const skillController = require('../controllers/skill.controller');
 
 const router = express.Router();
 
+// Mengambil seluruh daftar Master Skill
 router.get('/', (req, res, next) => skillController.getAll(req, res, next));
+
+// Mengambil satu Skill berdasarkan ID
 router.get('/:id', (req, res, next) => skillController.getById(req, res, next));
 
 module.exports = router;
